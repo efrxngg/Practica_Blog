b862e313aa9ef928d1f34752dd94928beb87f788
efren <efrenpgc2602@gmail.com>
2022-06-22 13:07:37 -0500

    correcion producto-9

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.contrib import admin
+
 urlpatterns=[
     path('', home, name = 'blog_index'),
     path('generales/', generales, name='blog_generales'),
@@ -7,7 +9,8 @@ urlpatterns=[
     path('videojuegos/', videojuegos, name = 'blog_videojuegos'),
     path('technologia/', technologia, name='blog_technologia'),
     path('tutoriales/', tutoriales, name='blog_tutoriales'),
-    
+    path('admin/', admin.site.urls),
+
     # Va al final 
     path('<slug:slug>/', detalle_post, name= 'blog_detalle_post'), 
     
